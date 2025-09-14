418152f2c547e83a28f2474fa8257a3ea6ccc083
Vineet <vineet.insights01@gmail.com>
2025-09-14 19:26:26 +0530

    fix GitHub Pages asset paths

--- 14-Scroll-based-animation/vite.config.js
+++ 14-Scroll-based-animation/vite.config.js
@@ -3,6 +3,7 @@ import restart from 'vite-plugin-restart'
 export default {
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    base: '/11-Scroll-based-animation/dist/',  // Add this line for GitHub Pages
     server:
     {
         host: true, // Open to local network and display URL
